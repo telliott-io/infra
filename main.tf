56400287b4d9ea1269506f9014a57c9b25ea052b
Tom Elliott <tom.w.elliott@gmail.com>
2019-11-20 22:13:03 -0500

    Remove OAuth token for Google Cloud

--- main.tf
+++ main.tf
@@ -1,10 +1,6 @@
 #####################################################################
 # Variables
 #####################################################################
-variable token {
-  description = "OAuth token for Google Cloud user. Can be obtained with `gcloud auth application-default print-access-token`"
-}
-
 variable cloudflare_email {}
 variable cloudflare_api_key {}
 variable cloudflare_zone_id {}
