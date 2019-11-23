d382ca74d2cf43b976317dc73120df625d25315b
Tom Elliott <tom.w.elliott@gmail.com>
2019-11-23 18:40:13 -0500

    Remove now-unused Google provider in ingress

--- ingress/nginx/provider.tf
+++ ingress/nginx/provider.tf
@@ -8,10 +8,3 @@ provider "kubernetes" {
   cluster_ca_certificate = "${base64decode(var.cluster_ca_certificate)}"
   token = "${data.google_client_config.current.access_token}"
 }
-
-provider "google" {
-  credentials = "${file("gcloud-credentials.json")}"
-  project     = "telliott-io"
-  region      = "us-central1"
-  zone        = "us-central1-c"
-}
