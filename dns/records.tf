19f1d9d15ac40735f8b38b9b4484bc2d4661657a
Tom Elliott <tom.w.elliott@gmail.com>
2019-12-08 18:12:14 -0500

    Add CloudFlare proxy for all domains

--- dns/records.tf
+++ dns/records.tf
@@ -4,6 +4,7 @@ resource "cloudflare_record" "ingress" {
   value   = "${var.ingress_address}"
   type    = "A"
   ttl     = 1
+  proxied = true
 }
 
 resource "cloudflare_record" "blogarchive" {
@@ -12,6 +13,7 @@ resource "cloudflare_record" "blogarchive" {
   value   = "ingress.telliott.io"
   type    = "CNAME"
   ttl     = 1
+  proxied = true
 }
 
 resource "cloudflare_record" "front" {
@@ -20,6 +22,7 @@ resource "cloudflare_record" "front" {
   value   = "ingress.telliott.io"
   type    = "CNAME"
   ttl     = 1
+  proxied = true
 }
 
 resource "cloudflare_record" "jaeger" {
@@ -28,6 +31,7 @@ resource "cloudflare_record" "jaeger" {
   value   = "ingress.telliott.io"
   type    = "CNAME"
   ttl     = 1
+  proxied = true
 }
 
 resource "cloudflare_record" "prometheus" {
@@ -36,6 +40,7 @@ resource "cloudflare_record" "prometheus" {
   value   = "ingress.telliott.io"
   type    = "CNAME"
   ttl     = 1
+  proxied = true
 }
 
 
@@ -45,4 +50,5 @@ resource "cloudflare_record" "grafana" {
   value   = "ingress.telliott.io"
   type    = "CNAME"
   ttl     = 1
+  proxied = true
 }
