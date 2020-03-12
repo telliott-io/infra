a75e26088a08a2b2bc2e31a2bffa8d8fda30c202
Tom Elliott <tom.w.elliott@gmail.com>
2020-03-11 23:32:53 -0400

    Add bootstrap helm repo
    This will pull in all applications automatically when an environment is deployed.

---
+++ cd/argocd/bootstrap.tf
@@ -0,0 +1,11 @@
+data "helm_repository" "bootstrap" {
+    name = "bootstrap"
+    url  = "https://telliott-io.github.io/bootstrap"
+}
+
+resource "helm_release" "bootstrap" {
+  name       = "bootstrap"
+  repository = data.helm_repository.bootstrap.metadata[0].name
+  chart      = "bootstrap/bootstrap"
+  namespace  = "argocd"
+}
