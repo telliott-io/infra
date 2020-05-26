a67679d4e0ec121cc3d9299cddad8ba12c2f1f1a
Tom Elliott <tom.w.elliott@gmail.com>
2020-05-25 23:14:56 -0400

    Generate signing certificates for each test run

--- secrets/secrets_test.go
+++ secrets/secrets_test.go
@@ -1,14 +1,20 @@
 package secrets
 
 import (
+	"bytes"
+	"crypto/rand"
 	"crypto/rsa"
+	"crypto/x509"
+	"crypto/x509/pkix"
 	"encoding/base64"
 	"encoding/json"
+	"encoding/pem"
 	"errors"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
+	"math/big"
 	"os"
 	"os/exec"
 	"path"
@@ -49,6 +55,11 @@ func TestSigning(t *testing.T) {
 	}
 	defer kindCleanup()
 
+	signingCert, signingKey, err := createCerts()
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	terraformOptions := &terraform.Options{
 		// The path to where your Terraform code is located
 		TerraformDir: tfdir,
@@ -66,7 +77,7 @@ func TestSigning(t *testing.T) {
 	result := terraform.InitAndApply(t, terraformOptions)
 	t.Logf("Stdout: %v", result)
 
-	ss, err := sealTestSecret()
+	ss, err := sealTestSecret(signingCert)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -112,7 +123,6 @@ func TestSigning(t *testing.T) {
 	if string(data) != secretValue {
 		t.Errorf("generated secret incorrect. expected %q, got %q", secretValue, string(data))
 	}
-
 }
 
 func setupWorkingDir(p string) (func() error, error) {
@@ -142,7 +152,7 @@ func execKubectl(tfdir, kubeconfigfile string, params []string) (string, error) 
 	return string(output), nil
 }
 
-func sealTestSecret() (*sealedsecrets.SealedSecret, error) {
+func sealTestSecret(signingCert string) (*sealedsecrets.SealedSecret, error) {
 	pubKey, err := parseKey(strings.NewReader(signingCert))
 	if err != nil {
 		return nil, err
@@ -206,96 +216,49 @@ func createKindCluster(kubeConfig string) (func() error, error) {
 	}, nil
 }
 
-const signingCert = `
------BEGIN CERTIFICATE-----
-MIIFoDCCA4gCCQCcwV/Fhn/3/TANBgkqhkiG9w0BAQsFADCBkTELMAkGA1UEBhMC
-VVMxCzAJBgNVBAgMAk5ZMREwDwYDVQQHDAhOZXcgWW9yazEUMBIGA1UECgwLdGVs
-bGlvdHQuaW8xDjAMBgNVBAsMBUluZnJhMRQwEgYDVQQDDAt0ZWxsaW90dC5pbzEm
-MCQGCSqGSIb3DQEJARYXdG9tLncuZWxsaW90dEBnbWFpbC5jb20wHhcNMjAwNTI0
-MTMxNDU0WhcNMjEwNTI0MTMxNDU0WjCBkTELMAkGA1UEBhMCVVMxCzAJBgNVBAgM
-Ak5ZMREwDwYDVQQHDAhOZXcgWW9yazEUMBIGA1UECgwLdGVsbGlvdHQuaW8xDjAM
-BgNVBAsMBUluZnJhMRQwEgYDVQQDDAt0ZWxsaW90dC5pbzEmMCQGCSqGSIb3DQEJ
-ARYXdG9tLncuZWxsaW90dEBnbWFpbC5jb20wggIiMA0GCSqGSIb3DQEBAQUAA4IC
-DwAwggIKAoICAQDNVf4Oxzb6tRmPNtxrYHjM8oSHOvP/9xsZkUY1qMN2CKPU8GzR
-8ZGgv3bZjW+UfgIm+/iFppkrM5adAQz+v6cwD9mY9rxBW9iJ6pAb2t1a4x35Zces
-Z8YTc902VdAchF2PXs/pLY/IcHm87vwh7mpvGqv58Vu6LwmovaMx+XALyyN+2e2l
-pq0vSXJy+vqnXjQbafim5qFy3sFoMIsNImyNY0ak7k0vQQQYKivDiT8u4XHd9klQ
-rsXwCwF+P6XcdFIxYeelAFv3g56jEh8be8TV1xKJvzqUOI4P/Gm9ynv6ovD1XsXE
-WilvnwBMaCug7riv04Bl2v7C4lGDT6NRXH+CfkC3UKo3sKZ5IjPvZ82cGubzYOym
-PGM6QuyDrAOPk/0/DvqN3QcS45GjOPvjENn//lAcldTmv65ktYnCh0I9da+gtbrY
-+3bdeIYyvzx2GW52aDvqUW6eWpGop5GtiTAmi4WU6e18q1fxhC+INILqtIv+9RWU
-dW9+9tBn/aE4i8yPcgLyNs4zF6Le8d0GMDkzX35vundC/KCg5r8xlknNARuWsc6d
-Zw2/okMZe9fOZ7EYlurPcr6o/0cN1+JO6o0ygT9gm/mp0i6GcWeDLZpzFAfgu5n3
-z0LmCBJRFHL7fgMbDezl5SV133curUwm6QThmtfCPsnYuCxS53WG23uGTwIDAQAB
-MA0GCSqGSIb3DQEBCwUAA4ICAQBWCCprg2sc0e4wQQh3Gtyx5pUjIkt0JbgvpC/6
-C5XzAmyHaYfOKrSm2XM84m6MBBN9f7I2G6cN2Fab4c9fIXMDqJhwKKfvJF3xq87r
-s1VoJRT67xd2TBx7Jk/v1AUZlvu2uV7Ku7uewCljV+gtJMtf6n5JmUAOEqU1eZ5k
-yWIHu0gigYjYSoDXeJJEVMFOzG2gVW0Nkwg+Pi49PLqqh2STXTY3EysURSzHCuxN
-g1LbkAL3u7gy7eCDOTddruYVd/ZphpxTnxwYQ2Y+7VkYyZEUM4l5uecn5Dl/7fD9
-+gA0xHrmMSUyov+Mo9XVOc1PYgaMj4Oho+ZKS4Nyt3N41txq4CRsue0wmiOKxSvf
-dvrbX3ZjWJqQmjQqJMWpWWfUSjSJbZL/VQg+Bj1q2t9mn8fQJzleUBwqd2/JEcu3
-MDTIss2vRPOAvLK8NYzzrIl2R0Hpl+fYKNfZNa3MaxUB+IM2Rv/k3lbIXyUQ6R+p
-NJN6o3sVn5qA3jiUD/yrMMbPV56KWYdS36Hq2d2s+2T3zIDWwr+fYryb+W/oiKFA
-sLr3jmpgWwCUrmcmthjRR4qJ0NaGtDGzOc6vUk7YbY8AoruG8J7K3Rj5F3/BSBw9
-RqcivwnkKB4zxzVjdwznUkfp5aaG7TRn8f4BNZu4TaNYRJloNW4xvrv+Gdo/3Xn7
-uIr1ZA==
------END CERTIFICATE-----
-`
+func createCerts() (crt string, key string, err error) {
+	ca := &x509.Certificate{
+		SerialNumber: big.NewInt(2019),
+		Subject: pkix.Name{
+			Organization: []string{"telliott.io"},
+			Country:      []string{"US"},
+			Province:     []string{"NY"},
+			Locality:     []string{"New York"},
+		},
+		NotBefore:             time.Now(),
+		NotAfter:              time.Now().AddDate(10, 0, 0),
+		IsCA:                  false,
+		ExtKeyUsage:           []x509.ExtKeyUsage{},
+		KeyUsage:              x509.KeyUsageDigitalSignature,
+		BasicConstraintsValid: true,
+	}
+	caPrivKey, err := rsa.GenerateKey(rand.Reader, 4096)
+	if err != nil {
+		return "", "", err
+	}
+	caBytes, err := x509.CreateCertificate(rand.Reader, ca, ca, &caPrivKey.PublicKey, caPrivKey)
+	if err != nil {
+		return "", "", err
+	}
+	caPEM := new(bytes.Buffer)
+	err = pem.Encode(caPEM, &pem.Block{
+		Type:  "CERTIFICATE",
+		Bytes: caBytes,
+	})
+	if err != nil {
+		return "", "", err
+	}
 
-const signingKey = `
------BEGIN PRIVATE KEY-----
-MIIJQgIBADANBgkqhkiG9w0BAQEFAASCCSwwggkoAgEAAoICAQDNVf4Oxzb6tRmP
-NtxrYHjM8oSHOvP/9xsZkUY1qMN2CKPU8GzR8ZGgv3bZjW+UfgIm+/iFppkrM5ad
-AQz+v6cwD9mY9rxBW9iJ6pAb2t1a4x35ZcesZ8YTc902VdAchF2PXs/pLY/IcHm8
-7vwh7mpvGqv58Vu6LwmovaMx+XALyyN+2e2lpq0vSXJy+vqnXjQbafim5qFy3sFo
-MIsNImyNY0ak7k0vQQQYKivDiT8u4XHd9klQrsXwCwF+P6XcdFIxYeelAFv3g56j
-Eh8be8TV1xKJvzqUOI4P/Gm9ynv6ovD1XsXEWilvnwBMaCug7riv04Bl2v7C4lGD
-T6NRXH+CfkC3UKo3sKZ5IjPvZ82cGubzYOymPGM6QuyDrAOPk/0/DvqN3QcS45Gj
-OPvjENn//lAcldTmv65ktYnCh0I9da+gtbrY+3bdeIYyvzx2GW52aDvqUW6eWpGo
-p5GtiTAmi4WU6e18q1fxhC+INILqtIv+9RWUdW9+9tBn/aE4i8yPcgLyNs4zF6Le
-8d0GMDkzX35vundC/KCg5r8xlknNARuWsc6dZw2/okMZe9fOZ7EYlurPcr6o/0cN
-1+JO6o0ygT9gm/mp0i6GcWeDLZpzFAfgu5n3z0LmCBJRFHL7fgMbDezl5SV133cu
-rUwm6QThmtfCPsnYuCxS53WG23uGTwIDAQABAoICABoRUK+Pmus5EoWb8V+wX6Wl
-JC4GzfMw67/TMZaeVjIl5qah3y7H/DTsGaqEyiWP2bYZRvOwssFJS1WjTIMK0a7I
-VFeW+09Q2ayomqLupitjqep/gVnh0sOlce/z9Pep1jbdUofHwPkxTkMuE9Q68Egq
-mqqgeYSpdBB4Ar/VauQUZe00vXbKMjJOoLj4I9obm4HfcjG+FcD/ho4zm3OPaziH
-4fAOUL4vAtYOhH/ObLcG5+3F1ojnpzlSLF5atjPGkKi0RTQtV3+Utg79vfU/QT3k
-j/Xs0QMAKwjcgpAVpKetB+oenfzPdXM2PFN25hMIO2oW77X+l1kmYjLJaXJqy8UL
-CglXBSgQVSyTR6czla1XeQO6R5fa+ju80L8Jfl+9E9//Kr/iIrlR35BXBqL4EHdf
-8rkX20wAyWPXs4SpC5jfc4FB2zVCS50gVaxkvvr6hJrK/iTWhbIGfFAseVh+1xhT
-UzTMPDAEhg9nN+Ey0gc2D9yu3zi7LgUFrbYwvSYpbP1cs5kJ8CDNk2J7VulwHgjp
-yfMHmYbrowxPmFXbQ9i7nweQaob8/VgzPlJ3/yW/cPd+bv+2aizw8L5RoVJKyaKL
-fqyiMFvq9WEInglHIc3IgrHTwAsIbOFvR3/JtG6yQoptLdxLiiR31Ma1R+oBMwPx
-sbXG0Qf3wm3lVv/171wBAoIBAQDociMGpYhXA4c/a2/x6g0dMyu16dd1XiKvvmpC
-uhHJd8j07qp3WrLZG7F+U57CHfyexJyQ9ntFUdyYAeFb/U+rQp/6+vDqGMuWBLGi
-ecHUhhCePEyBUJHXODi70baD6YsCBDT/fUbxKR5WhGHvIdhzykhzeTzJjqcVst6X
-qLBg7sGrJpjy5svGTB9euhL6oWn4g+tBTUbRW+sq1SqoBuvp+8+WvGkiEbsm/QvI
-mY8ci/5GpYqHjWwmUPmhnQY0N90QiO0xt5dpa7zmLLPtXlkjweksFxn3F5fNjil8
-BPaf4nisGXJbw2jhtlFqlfV2d1Kkb1631BkaP+CCHoRynuHtAoIBAQDiJJkuAMZR
-2DWQcA1kQwmp0HtCSxx7oe5/GojZnOZM3ya/IZQ7QTEhU2okSe2zjULeP+YX7wof
-HlvGKnP7gep/NtPDuE2PXxKAc1P94zR4HuFe5It0OVroUtUQ31HYfQiCQhv+l7W0
-lJydsMxXfsscmcBkMsEykBk+E9FSqwb7A/61mbXlGUnZlEj3xUO7RUsheaF4xxOk
-EcH13Q8wbJo24qIzITzmU/scDr1CdHhx1KSro9lt14jQK8NmhyHwjQvtuKmK18qt
-Jtlb/y6ZfmlgAy/6SM/MmkOEFmYU2o9+FuPeH/W1fhyvMSfIK2Jl2DsgFHdzDuTD
-BONjOFLABXGrAoIBAQDaH7kElLNjS/lPpwcOkR6rRwh+lahNCAAuwNXANhFY6GIY
-jhpDFEZ+e4FM1TVGXZNgfYmacuHsg8wojKMoioWHZWbwwyjt1ES/SzpSCEW+o+vW
-QSlds/iXaLe8cCgAk1iRlNcS5UF8LqS8dU/dcfpHdgwU6DEFxyq9rvM4h+CzpXjx
-na+rSK7yLx7E6oHC1VO3FDvgNJ7++W2t7QkxUgVxtY3wmm9hvBfOl2jGXmswU6JS
-MyfS2gAh2JzYbpySxdVZndRPckPQdnx11n8lgQLPDjk72ZXRDD+0cfI14gZ4tLGe
-deade7rhDBBMn9oOyBwyGBxfDtZbjpOkJvUNBlFlAoIBABEIQKE+XbF8X0xppBiR
-FAE+OopbreGB6LyZ+wSo1I+lnv12QfvUhSbFaZEiIE0NrqOenG3hCxoc5zydpe6j
-ck3yizCKiQzrVDFofkL5AdKqQL8s53Pxfe9Rjcqfh0KO6D0nTYR3WLApIUKfNkTA
-v/t/eQYS7IoqYDxUscdQKk5tlpmG4jRHG1DP4k8HBHruVSJITukR1WDeFYW3CJXV
-GChaGW1Qa43NdXQ/h/GhZBDuuxhSVuX2/c7v/N+T/fJoLSXSarKSXil8a5KKPbBB
-3R3mH47yPPRkCgHAzh9z9qrAfpkUPyVUQkTPpvdkjRyulIVuBTEdRg2KKLYoX9Mm
-ldECggEAQbv3UZI8qgZmclhRLbVaKp2zmpyP9P3p2qgX9J8MLjWYa1Yv/cj65k+V
-oS0PHFeVzp4ZdiEjom3qFWs343/BH+QezA5pZFy6Jn0b8zafA+dgDqWTZf3CjQT1
-YUNIM0GBr8sKUljsI8zLzvF6oVGhJY3Ew47fImC32U991GsQvirbI2e0Pjl9lfPR
-hcHmsWxiETitGmRjXZfhatR3bM/Ieb29Cp4zTKWbAx0duGZn2M9IMREg7ltnEzJ6
-fwSctWMSe6RPexYgn0MhLg7qsGaeNAldiuNavyAnw6Bu3ki0t3kuzqyCD8d8ECab
-0UuyW6PdDTYvKVmj9+wNHe+dkK4Wlg==
------END PRIVATE KEY-----
-`
+	caPrivKeyPEM := new(bytes.Buffer)
+	err = pem.Encode(caPrivKeyPEM, &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(caPrivKey),
+	})
+	if err != nil {
+		return "", "", err
+	}
+	return string(caPEM.Bytes()), string(caPrivKeyPEM.Bytes()), nil
+}
 
 const secretValue = "bar"
 
