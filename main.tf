29caedd728dcc423f0f250337dc5bc2022144349
Tom Elliott <tom.w.elliott@gmail.com>
2020-05-29 09:53:19 -0400

    Fix variable names

--- main.tf
+++ main.tf
@@ -5,8 +5,8 @@
 module "do" {
   source   = "./environments/do"
   argocd_admin_password = var.argocd_admin_password
-  signing_cert = var.secret_signing_cert
-  signing_key = var.secret_signing_key
+  secret_signing_cert = var.secret_signing_cert
+  secret_signing_key = var.secret_signing_key
 }
 
 module "dns" {
