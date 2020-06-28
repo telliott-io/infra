b82f164415628a1edc43ea8a194e8554842750df
Tom Elliott <tom.w.elliott@gmail.com>
2020-06-27 20:53:49 -0400

    Remove do-prod-a

--- terragrunt/clusters/do-prod-a.tf
+++
@@ -1,9 +0,0 @@
-module "do-prod-a" {
-  source   = "github.com/telliott-io/kube-clusters//digitalocean?ref=v0.2.0"
-  cluster_name = "do-prod-a"
-}
-
-output "do-prod-a" {
-  value = module.do-prod-a.kubernetes
-  sensitive = true
-}
