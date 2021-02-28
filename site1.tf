634a099cb1ce000c817837478a8af4d9a2a4940a
Tom Elliott <tom.w.elliott@gmail.com>
2021-02-27 22:55:13 -0500

    Replace site1

---
+++ site1.tf
@@ -0,0 +1,16 @@
+module "cluster" {
+  source   = "github.com/telliott-io/kube-clusters//digitalocean?ref=v0.6.1"
+  cluster_name = "site1"
+}
+
+module "platform" {
+  source = "github.com/telliott-io/platform?ref=v0.6.0"
+	kubernetes = jsonencode(module.cluster.kubernetes)
+	environment = "platform-test"
+	hostname = "platform.test"
+	argocd_admin_password = "secret"
+	bootstrap_repository = "https://telliott-io.github.io/testbootstrap"
+	bootstrap_chart = "bootstrap"
+	bootstrap_version = "0.1.1"
+	dns_name = var.dns_name
+}
