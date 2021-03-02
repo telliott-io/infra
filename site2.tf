29e9d7beba10621032d0717bd1d1184140e13779
Tom Elliott <tom.w.elliott@gmail.com>
2021-03-01 22:08:00 -0500

    Add a second site

---
+++ site2.tf
@@ -0,0 +1,7 @@
+module "site2" {
+  source   = "./site"
+  cluster_name = "site2"
+  argocd_admin_password = var.argocd_admin_password
+  vcs_oauth_token_id = var.vcs_oauth_token_id
+  dns_name = var.dns_name
+}
