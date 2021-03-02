bb963662e397e79f41d178e52bcc1f4bcdc8e40b
Tom Elliott <tom.w.elliott@gmail.com>
2021-03-01 22:29:39 -0500

    Remove second site

--- site2.tf
+++
@@ -1,7 +0,0 @@
-module "site2" {
-  source   = "./site"
-  cluster_name = "site2"
-  argocd_admin_password = var.argocd_admin_password
-  vcs_oauth_token_id = var.vcs_oauth_token_id
-  dns_name = var.dns_name
-}
